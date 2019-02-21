24a51f51f1f9c4ed8e6e9912e8098f976647ba49
Steven Hill <smhill001@gmail.com>
2019-02-21 13:27:36 -0700

    Minor Refactor
    Keeps it working with the Utils repo

--- PlanetMapScript.py
+++ PlanetMapScript.py
@@ -5,10 +5,30 @@ Created on Thu Nov 08 12:17:15 2018
 @author: Steven Hill
 """
 import PlanetMapTest as PMT
-PMT.PlanetMapTest("Jupiter","Jupiter")
-PMT.PlanetMapTest("Mars","Mars")
-PMT.PlanetMapTest("Jupiter","Jupiter-2013-RGB")
-PMT.PlanetMapTest("Jupiter","Jupiter-NEB-Grey")
-PMT.PlanetMapTest("Jupiter","Jupiter-NEB-RGB")
-PMT.PlanetMapTest("Jupiter","Jupiter-NEB-RED")
-PMT.PlanetMapTest("Jupiter","Jupiter-2013-RGB")
+
+def Test():
+
+    PMT.PlanetMapTest("Jupiter","Jupiter")
+    PMT.PlanetMapTest("Mars","Mars")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2013-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-NEB-Grey")
+    PMT.PlanetMapTest("Jupiter","Jupiter-NEB-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-NEB-RED")
+    return 0
+    
+def JupiterGlobalMap():
+
+    PMT.PlanetMapTest("Jupiter","Jupiter-2005-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2006-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2007-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2008-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2009-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2010-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2012-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2013-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2014-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2015-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2016-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2017-RGB")
+    PMT.PlanetMapTest("Jupiter","Jupiter-2018-RGB")
+    return 0
--- PlanetMapTest.py
+++ PlanetMapTest.py
@@ -11,11 +11,10 @@ def PlanetMapTest(Target,PlotID):
     sys.path.append(drive+'\\Astronomy\Python Play')
     sys.path.append(drive+'\\Astronomy\Python Play\Util')
     sys.path.append(drive+'\\Astronomy\Python Play\SpectroPhotometry\Spectroscopy')
-    
-    #import matplotlib.pyplot as pl
-    #import SysRespLIB as SRL
+    import matplotlib.path as mpath
+
     import ConfigFiles as CF
-    import GeneralSpecUtils as GSU
+    import PlotUtils as PU
     import cartopy.crs as ccrs
     import scipy.ndimage as nd
     import pylab as pl
@@ -23,68 +22,53 @@ def PlanetMapTest(Target,PlotID):
     import exifread
     
     drive="f:"
-    #Target="Mars"
     path="/Astronomy/Projects/Planets/"+Target+"/Imaging Data/Mapping/"
-    #fn="2018MarsMaster-Bare.png"
     
-    #f=drive+path+fn
-    MapSetup=CF.PlotSetup("f:/Astronomy/Python Play/PlanetMaps/MapConfig.txt")
+    MapSetup=PU.PlotSetup("f:/Astronomy/Python Play/PlanetMaps/MapConfig.txt")
     MapSetup.loadplotparams(drive,PlotID,"Map")
-    
-    MapSetup.Setup_Map()
-    
+    fig=pl.figure(figsize=(6.0, 6.0), dpi=150, facecolor="white")
+    MapSetup.Setup_CaratoPy_Map("PC",2,1,1)
     if MapSetup.ColorPlane=="Grey":
         test=nd.imread(drive+path+MapSetup.DataFile,flatten=True)
-        pl.imshow(test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:],"gray")
     elif MapSetup.ColorPlane<>"Grey":
         test=nd.imread(drive+path+MapSetup.DataFile,flatten=False)
-        if MapSetup.ColorPlane=="RGB":
-            pl.imshow(test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:])
-        if MapSetup.ColorPlane=="RED":
-            pl.imshow(test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,0],"gray")
-        if MapSetup.ColorPlane=="GRN":
-            pl.imshow(test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,1],"gray")
-        if MapSetup.ColorPlane=="BLU":
-            pl.imshow(test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,2],"gray")
-        
-    #pl.imshow(test[0:90,:],"gray")
-    pl.subplots_adjust(left=0.10, bottom=0.14, right=0.98, top=0.92,
-                wspace=None, hspace=None)
-    pl.savefig(drive+path+PlotID+".png",dpi=300)
-    
-    ###########################################################Start of CartoPy
-    #ax.gridliner(x,)
-    #ax.xtick_labels(-np.linspace(-180,180,13))
-    
-    MapSetup.Setup_CaratoPy_Map()
     
     test_crs = ccrs.PlateCarree()
     test_extent=[-180,180,int(MapSetup.Y0),int(MapSetup.Y1)]
     print "BEFORE CONDITIONAL"
-    if len(test.shape)>2: 
-        test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,:]
+    print test.shape
+    if len(test.shape)>2:
+        print MapSetup.ColorPlane
+        if MapSetup.ColorPlane=="RGB":
+            test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,:]
+        if MapSetup.ColorPlane=="RED":
+            test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,0]
+        if MapSetup.ColorPlane=="GRN":
+            test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,1]
+        if MapSetup.ColorPlane=="BLU":
+            test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:,2]      
     else:
-        test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:]
+        if MapSetup.ColorPlane=="Grey":
+            test_patch=test[90-int(MapSetup.Y1):90-int(MapSetup.Y0),:]
         
     print "test_extent=",test_extent
     pl.imshow(test_patch, "gray",origin='upper', transform=ccrs.PlateCarree(), extent=test_extent)
     
-    pl.subplots_adjust(left=0.10, bottom=0.14, right=0.98, top=0.92,
+    pl.subplots_adjust(left=0.08, bottom=0.14, right=0.98, top=0.92,
                 wspace=None, hspace=None)
+    ###########################################################################
+   
+    #test_extent=[-180.,180.,-90.,90]
     
-    lon = np.linspace(-180, 180, 360)
-    lat = np.linspace(90, -90, 180)
-    #lon2d, lat2d = np.meshgrid(lon, lat)
-    
-    test_extent=[-180.,180.,-90.,90]
-    
-    fig1=pl.figure(figsize=(6., 3.), dpi=150, facecolor="white")
-    ax1 = pl.axes(projection=ccrs.PlateCarree())
-    ax1.set_extent([-180, 180, -90, 20], crs=ccrs.PlateCarree())
-    ax1.gridlines(crs=test_crs,linewidth=0.2)
-    ax1.set_xticks(np.arange(-180,179,30), minor=False, crs=ccrs.PlateCarree())
-    ax1.imshow(test,transform=test_crs,extent=test_extent,origin="upper",cmap="gray")  
-    
+    #fig1=pl.figure(figsize=(6., 3.), dpi=150, facecolor="white")
+
+    MapSetup.Setup_CaratoPy_Map("NP",2,2,3)
+    pl.imshow(test_patch,transform=test_crs,extent=test_extent,origin="upper",cmap="gray")  
+
+    MapSetup.Setup_CaratoPy_Map("SP",2,2,4)  
+    pl.imshow(test_patch,transform=test_crs,extent=test_extent,origin="upper",cmap="gray")  
+
+    pl.savefig(drive+path+PlotID+".png",dpi=300)
     
     #x=np.zeros([180,360])
     #x[40:60,100:300]=test[40:60,100:300]
@@ -93,4 +77,5 @@ def PlanetMapTest(Target,PlotID):
     
     #exiftest = open(f, 'rb')
     #tags = exifread.process_file(exiftest)
+    
     return 0
